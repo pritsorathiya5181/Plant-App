5fe0387a07c046f9fab180cad29172bb830166f1
prit <prit.sorathiya@gmail.com>
2020-06-27 13:58:16 +0530

    "initial commit with required assets"

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:Plant_app/constants.dart';
+import 'package:Plant_app/screens/home/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        scaffoldBackgroundColor: kBackgroundColor,
+        primaryColor: kPrimaryColor,
+        textTheme: Theme.of(context).textTheme.apply(bodyColor: kTextColor),
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
